431c0ea3ff95daaa0389bc2d086c0100d4c93cce
Niggi1105 <niklas.liesch17@gmail.com>
2023-02-21 15:39:37 +0100

    modified:   Cargo.lock 	modified:   Cargo.toml 	modified:   src/lib.rs 	new file:   src/opcodes.rs 	modified:   target/.rustc_info.json

---
+++ src/opcodes.rs
@@ -0,0 +1,69 @@
+
+extern crate lazy_static;
+use std::collections::HashMap;
+use crate::cpu::AddressingMode;
+
+pub struct OpCode{
+    pub op: u8,
+    pub name: &'static str,
+    pub bytes: u8,
+    pub cycles: u8,
+    pub addr_mode: AddressingMode,
+}
+impl OpCode {
+    pub fn new(opc: u8, na: &'static str, bt: u8, cy: u8, admode: AddressingMode) -> Self {
+        OpCode { op: opc, name: na, bytes: bt, cycles: cy, addr_mode: admode}
+    }
+
+}
+lazy_static::lazy_static!{
+    pub static ref CPU_OPCODES: Vec<OpCode> = vec![
+        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
+        
+        OpCode::new(0xAA, "TAX", 1, 2, AddressingMode::NoneAddressing),
+
+        OpCode::new(0xA0, "LDY", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xA4, "LDY", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xB4, "LDY", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0xAC, "LDY", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0xBC, "LDY", 3, 4, AddressingMode::AbsoluteX),
+
+        OpCode::new(0xA2, "LDX", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xA6, "LDX", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xB6, "LDX", 2, 4, AddressingMode::ZeroPageY),
+        OpCode::new(0xAE, "LDX", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0xBE, "LDX", 3, 4, AddressingMode::AbsoluteY),
+
+        OpCode::new(0xA9, "LDA", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xA5, "LDA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xB5, "LDA", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0xAD, "LDA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0xBD, "LDA", 3, 4, AddressingMode::AbsoluteX),
+        OpCode::new(0xB9, "LDA", 3, 4, AddressingMode::AbsoluteY),
+        OpCode::new(0xA1, "LDA", 2, 6, AddressingMode::IndirectX),
+        OpCode::new(0xB1, "LDA", 2, 5, AddressingMode::IndirectY),
+
+        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0x8D, "STA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x9D, "STA", 3, 5, AddressingMode::AbsoluteX),
+        OpCode::new(0x81, "STA", 2, 6, AddressingMode::IndirectX),
+        OpCode::new(0x91, "STA", 2, 6, AddressingMode::IndirectY),
+
+        OpCode::new(0x84, "STY", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x94, "STY", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0x8C, "STY", 3, 4, AddressingMode::Absolute),
+
+        OpCode::new(0x86, "STX", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x96, "STX", 2, 4, AddressingMode::ZeroPageY),
+        OpCode::new(0x8E, "STX", 3, 4, AddressingMode::Absolute)
+    ];
+
+    pub static ref OP_MAP: HashMap<u8,&'static OpCode> = {
+        let mut map = HashMap::new();
+        for cpu_op in &*CPU_OPCODES{
+            map.insert(cpu_op.op, cpu_op);
+        }
+        map
+    };
+}
